d189ddcc1d5e013fe064200136a39de3ac315283
Mirac Orhan <mirac.orhan@gmail.com>
2025-02-04 16:43:25 +0100

    Refactor main.py to implement Ui_MainWindow class and load UI from mavi.ui

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
+from PyQt6 import QtCore, QtGui, QtWidgets,uic
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
-from login_page import LoginPage
-from user_list_page import UserListPage
-from book_list_page import BookListPage
+class Ui_MainWindow(QtWidgets.QMainWindow):
 
-class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle("PyQt6 Python Projesi")
-        self.setGeometry(100, 100, 400, 300)
-
-        self.stacked_widget = QStackedWidget()
-        self.setCentralWidget(self.stacked_widget)
-
-        # Sayfaları oluştur ve stacked_widget'e ekle
-        self.login_page = LoginPage(self.stacked_widget)
-        self.user_list_page = UserListPage(self.stacked_widget)
-        self.book_list_page = BookListPage(self.stacked_widget)
-
-        self.stacked_widget.addWidget(self.login_page)
-        self.stacked_widget.addWidget(self.user_list_page)
-        self.stacked_widget.addWidget(self.book_list_page)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+        super(Ui_MainWindow, self).__init__()
+        uic.loadUi('mavi.ui', self)
+        self.show()
+        self.pushButton_2.clicked.connect(self.pager)
+    def pager(self):
+            from yesil import Yesil_window
+            self.cams = Yesil_window()
+            self.cams.show()
+            self.close()
